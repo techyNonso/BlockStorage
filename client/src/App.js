5de853182de565fa63b3bf86a2957ddd5c3098c3
techyNonso <williamikeji@gmail.com>
2022-01-05 09:11:27 -0800

    front end done

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import BlockStorage from "./contracts/BlockStorage.json";
 import getWeb3 from "./getWeb3";
 
 import "./App.css";
+import Navbar from "./components/NavBar";
+import Main from "./components/Main";
+
+//Declare IPFS
+import { create } from "ipfs-http-client";
+
+/* Create an instance of the client */
+const client = create("https://ipfs.infura.io:5001/api/v0");
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = {
+    web3: null,
+    accounts: null,
+    contract: null,
+    filesCount: 0,
+    files: [],
+    buffer: null,
+    type: null,
+    name: null,
+    loading: false,
+  };
 
   componentDidMount = async () => {
     try {
@@ -17,56 +35,113 @@ class App extends Component {
 
       // Get the contract instance.
       const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
+      const deployedNetwork = BlockStorage.networks[networkId];
+      if (deployedNetwork) {
+        const instance = new web3.eth.Contract(
+          BlockStorage.abi,
+          deployedNetwork && deployedNetwork.address
+        );
 
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+        // Set web3, accounts, and contract to the state, and then proceed with an
+        // example of interacting with the contract's methods.
+        this.setState({ web3, accounts, contract: instance });
+
+        // Get files amount
+        const filesCount = await instance.methods.fileCount().call();
+        this.setState({ filesCount });
+        // Load files&sort by the newest
+        for (var i = filesCount; i >= 1; i--) {
+          const file = await instance.methods.files(i).call();
+          this.setState({
+            files: [...this.state.files, file],
+          });
+        }
+      } else {
+        alert("App does not exeist on this network");
+      }
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
+        `Failed to load web3, accounts, or contract. Check console for details.`
       );
       console.error(error);
     }
   };
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
+  // Get file from user
+  captureFile = (event) => {
+    event.preventDefault();
 
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
+    const file = event.target.files[0];
+    const reader = new window.FileReader();
 
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
+    reader.readAsArrayBuffer(file);
+    reader.onloadend = () => {
+      this.setState({
+        buffer: Buffer(reader.result),
+        type: file.type,
+        name: file.name,
+      });
+      console.log("buffer", this.state.buffer);
+    };
+  };
 
-    // Update state with the result.
-    this.setState({ storageValue: response });
+  uploadFile = async (description) => {
+    console.log("Submitting file to IPFS...");
+
+    // upload the file
+    const added = await client.add(this.state.buffer);
+
+    this.setState({ loading: true });
+    // Assign value for the file without extension
+    if (this.state.type === "") {
+      this.setState({ type: "none" });
+    }
+    this.state.contract.methods
+      .uploadFile(
+        added.path,
+        added.size,
+        this.state.type,
+        this.state.name,
+        description
+      )
+      .send({ from: this.state.accounts[0] })
+      .on("transactionHash", (hash) => {
+        this.setState({
+          loading: false,
+          type: null,
+          name: null,
+        });
+        window.location.reload();
+      })
+      .on("error", (e) => {
+        window.alert("Error");
+        this.setState({ loading: false });
+      });
   };
 
   render() {
     if (!this.state.web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
+    } else {
+      return (
+        <div className="App">
+          <Navbar account={this.state.accounts[0]} />
+          {this.state.loading ? (
+            <div id="loader" className="text-center mt-5">
+              <p>Loading...</p>
+            </div>
+          ) : (
+            <Main
+              files={this.state.files}
+              captureFile={this.captureFile}
+              uploadFile={this.uploadFile}
+              account={this.state.accounts[0]}
+            />
+          )}
+        </div>
+      );
     }
-    return (
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 42</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
-      </div>
-    );
   }
 }
 
---
+++ client/src/components/Main.js
@@ -0,0 +1,151 @@
+import React, { Component } from "react";
+import { convertBytes } from "./helpers";
+import moment from "moment";
+
+class Main extends Component {
+  render() {
+    return (
+      <div className="container-fluid mt-5 text-center">
+        <div className="row">
+          <main
+            role="main"
+            className="col-lg-12 ml-auto mr-auto  m-auto"
+            style={{ maxWidth: "1024px" }}
+          >
+            <div className="content border mr-auto ml-auto">
+              <p>&nbsp;</p>
+              <div
+                className="card mb-3 mx-auto "
+                style={{ maxWidth: "512px", backgroundColor: "#0077b6" }}
+              >
+                <h2 className="text-white text-monospace ">
+                  <b>
+                    <ins>Share File</ins>
+                  </b>
+                </h2>
+                <form
+                  onSubmit={(event) => {
+                    event.preventDefault();
+                    const description = this.fileDescription.value;
+                    this.props.uploadFile(description);
+                  }}
+                >
+                  <div className="form-group">
+                    <br></br>
+                    <input
+                      id="fileDescription"
+                      type="text"
+                      ref={(input) => {
+                        this.fileDescription = input;
+                      }}
+                      className="form-control text-monospace"
+                      placeholder="description..."
+                      required
+                    />
+                  </div>
+                  <input
+                    type="file"
+                    onChange={this.props.captureFile}
+                    className="text-white text-monospace"
+                  />
+                  <button
+                    type="submit"
+                    className="btn-primary btn-block bg-dark"
+                  >
+                    <b>Upload!</b>
+                  </button>
+                </form>
+              </div>
+              <p>&nbsp;</p>
+              <div className="table-responsive w-100">
+                <table
+                  className="table-sm table-bordered text-monospace table-striped"
+                  style={{ maxHeight: "450px" }}
+                >
+                  <thead style={{ fontSize: "15px" }}>
+                    <tr className="bg-dark text-white">
+                      <th scope="col" style={{ width: "10px" }}>
+                        id
+                      </th>
+                      <th scope="col" style={{ width: "200px" }}>
+                        name
+                      </th>
+                      <th scope="col" style={{ width: "230px" }}>
+                        description
+                      </th>
+                      <th scope="col" style={{ width: "120px" }}>
+                        type
+                      </th>
+                      <th scope="col" style={{ width: "90px" }}>
+                        size
+                      </th>
+                      <th scope="col" style={{ width: "90px" }}>
+                        date
+                      </th>
+                      <th scope="col" style={{ width: "120px" }}>
+                        uploader/view
+                      </th>
+                      <th scope="col" style={{ width: "120px" }}>
+                        hash/view/get
+                      </th>
+                    </tr>
+                  </thead>
+                  {this.props.files.map((file, key) => {
+                    return (
+                      <thead style={{ fontSize: "12px" }} key={key}>
+                        <tr>
+                          <td>{file.fileId}</td>
+                          <td>{file.fileName}</td>
+                          <td>{file.fileDescription}</td>
+                          <td>{file.fileType}</td>
+                          <td>{convertBytes(file.fileSize)}</td>
+                          <td>
+                            {moment
+                              .unix(file.uploadTime)
+                              .format("h:mm:ss A M/D/Y")}
+                          </td>
+                          <td>
+                            <a
+                              href={
+                                "https://etherscan.io/address/" + file.uploader
+                              }
+                              rel="noopener noreferrer"
+                              target="_blank"
+                            >
+                              {file.uploader.substring(0, 10)}...
+                            </a>
+                          </td>
+                          <td>
+                            {this.props.account == file.uploader && (
+                              <a
+                                href={
+                                  "https://ipfs.infura.io/ipfs/" + file.fileHash
+                                }
+                                rel="noopener noreferrer"
+                                target="_blank"
+                              >
+                                <button className="btn btn-primary btn-sm">
+                                  Download
+                                </button>
+                              </a>
+                            )}
+
+                            {this.props.account != file.uploader && (
+                              <span>file.fileHash.substring(0, 10)...</span>
+                            )}
+                          </td>
+                        </tr>
+                      </thead>
+                    );
+                  })}
+                </table>
+              </div>
+            </div>
+          </main>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Main;
