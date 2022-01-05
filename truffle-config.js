e0800e9d3ccaac24959ccc5c74c446eb25aa50a8
techyNonso <williamikeji@gmail.com>
2022-01-05 11:12:34 -0800

    deployed to ropsten and building on netlify

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,11 @@
 const path = require("path");
 
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const infuraKey = "b52b9f0144994a6497e6154d8b1be363";
+
+const fs = require("fs");
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -7,6 +13,17 @@ module.exports = {
   networks: {
     develop: {
       port: 8545,
+    },
+    ropsten: {
+      provider: new HDWalletProvider(
+        mnemonic,
+        `https://ropsten.infura.io/v3/${infuraKey}`
+      ),
+      network_id: 3,
+      gas: 5500000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
     },
   },
   compilers: {
