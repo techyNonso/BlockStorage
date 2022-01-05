4fbff23dbb9c0cb7d855d73e5fc08fc0f78171bf
techyNonso <williamikeji@gmail.com>
2022-01-05 11:32:47 -0800

    build uncomment

---
+++ client/build/precache-manifest.b0bb611a97f139a3446882bff950d3bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cd5ddbebbd1f30fd82ecfc1ac4f6f04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6f43df9a54b43e81754",
+    "url": "/static/css/2.b6834657.chunk.css"
+  },
+  {
+    "revision": "31dd54a55f0cb244e547",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b6f43df9a54b43e81754",
+    "url": "/static/js/2.72b62639.chunk.js"
+  },
+  {
+    "revision": "31dd54a55f0cb244e547",
+    "url": "/static/js/main.5046f8d6.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  },
+  {
+    "revision": "5aa8aa751d2b0db874ff66287d0f1bcc",
+    "url": "/static/media/box.5aa8aa75.png"
+  }
+]);
